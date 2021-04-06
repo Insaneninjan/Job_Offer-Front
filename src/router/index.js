8a9436d010d4363bbc34e76f9e310c8765e0f3b3
Nao <beerjerkkingk@gmail.com>
2021-04-06 23:30:39 +0900

    front end
    Add to GitHub

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Job from "../views/Job.vue";
+import JobEditor from '../views/JobEditor.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/job/:id',
+    name: 'job',
+    component: Job,
+    props: true,
   },
+  {
+    path: '/editor',
+    name: 'editor',
+    component: JobEditor,
+    props: true,
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes,
 });
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div>
+		<v-container>
+			<div v-for="job in jobs" :key="job.pk">
+				<h2>
+					<router-link
+						:to="{ name: 'job', params: { id: job.id } }"
+						class="job-link"
+					>{{ job.company_name }}
+					</router-link>
+				</h2>
+				<p>{{ job.job_title }}</p>
+				<hr>
+			</div>
+			<div class="my-4">
+				<p v-show="loadingJobs">...loading...</p>
+				<v-btn
+					v-show="next"
+					@click="getJobs"
+					color="success"
+				>Load More
+				</v-btn>
+			</div>
+		</v-container>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { apiService } from "../common/api.service.js"
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+	name: "home",
+	data() {
+		return {
+			jobs: [],
+      next: null,
+      loadingJobs: false
+		}
+	},
+	methods: {
+		getJobs() {
+			let endpoint = "/api/jobs/";
+      if(this.next) {
+        endpoint = this.next;
+      }
+      this.loadingJobs = true;
+			apiService(endpoint).then(data => {
+        this.jobs.push(...data.results)
+        this.loadingJobs = false;
+        if(data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
+
+        			})	
+		}
+	},
+	created() {
+		this.getJobs()
+    // 画面タイトル
+    document.title = "Job Board";
+	}
+}
 </script>
+
+<style scoped>
+.job-link {
+	font-weight: bold;
+	color: black;
+	text-decoration: none;
+}
+.job-link:hover {
+	color: #41B883;
+}
+</style>
